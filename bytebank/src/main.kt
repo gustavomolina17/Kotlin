bf26c168346c905abd85292ff18ffb12c19224dc
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-12-16 16:44:13 -0300

    2º Commit do Projeto

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,3 +1,19 @@
 fun main() {
     println("Bem vindo ao ByteBank");
+    val titular = "Gustavo Molina";
+    val numeroConta = 1208;
+    var saldo = 5000.0;
+    saldo += 3000;
+    // saldo = 0.0;
+    //saldo -=10000;
+
+    println("Titular da conta: $titular");
+    println("Nº da Conta: $numeroConta");
+    println("Saldo: $saldo");
+
+    when {
+        saldo > 0.0 -> println("Conta é Positiva");
+        saldo == 0.0 -> println("Conta é Neutra");
+        else -> println("Conta é Negativa");
+    }
 }
