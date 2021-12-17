73249865acf4d476e650bd8e8331951155e0e1e2
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-12-17 16:30:41 -0300

    3º Commit - Criação da Classe Conta

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,19 +1,64 @@
 fun main() {
-    println("Bem vindo ao ByteBank");
-    val titular = "Gustavo Molina";
-    val numeroConta = 1208;
-    var saldo = 5000.0;
-    saldo += 3000;
-    // saldo = 0.0;
-    //saldo -=10000;
+    println("Bem vindo ao ByteBank")
+    
+    val contaMolina = Conta()
+    contaMolina.titular = "Molina"
+    contaMolina.numero = 1010
+    contaMolina.saldo = 1000000.0
 
-    println("Titular da conta: $titular");
-    println("Nº da Conta: $numeroConta");
-    println("Saldo: $saldo");
+    val contaGustavo = Conta()
+    contaGustavo.titular = "Gustavo"
+    contaGustavo.numero = 1012
+    contaGustavo.saldo = 5000.0
 
+    println(contaGustavo.titular)
+    println(contaGustavo.numero)
+    println(contaGustavo.saldo)
+
+    println(contaMolina.titular)
+    println(contaMolina.numero)
+    println(contaMolina.saldo)
+
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
+}
+
+fun testaLacos(){
+    var i = 0
+    while(i < 3){
+        val titular: String = "Molina $i"
+
+            i++
+        }
+
+for (i in 3 downTo 1) {
+    val titular: String = "Molina $i"
+    val numeroConta: Int = 1000 + i
+    var saldo = i + 10.0
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+    println()
+}
+}
+
+fun testaCondicoes(saldo: Double) {
+    if (saldo > 0.0) {
+        println("conta é positiva")
+    } else if (saldo == 0.0) {
+        println("conta é neutra")
+    } else {
+        println("conta é negativa")
+    }
     when {
-        saldo > 0.0 -> println("Conta é Positiva");
-        saldo == 0.0 -> println("Conta é Neutra");
-        else -> println("Conta é Negativa");
+        saldo > 0.0 -> println("conta é positiva")
+        saldo == 0.0 -> println("conta é neutra")
+        else -> println("conta é negativa")
     }
 }
+
