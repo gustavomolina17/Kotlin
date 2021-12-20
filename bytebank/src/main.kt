fea70451a1a68bfdc04ed412f8526484466d8d63
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-12-20 14:34:13 -0300

    Atualizando o código

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,54 +1,54 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val contaAlex = Conta(titular = "Alex", numero = 1000)
-    contaAlex.deposita(200.0)
+    val contaGustavo = Conta(titular = "Gustavo", numero = 1000)
+    contaGustavo.deposita(500.0)
 
-    val contaFran = Conta(numero = 1001, titular = "Fran")
-    contaFran.deposita(300.0)
+    val contaMolina = Conta(numero = 1001, titular = "Molina")
+    contaMolina.deposita(800.0)
 
-    println(contaFran.titular)
-    println(contaFran.numero)
-    println(contaFran.saldo)
+    println(contaMolina.titular)
+    println(contaMolina.numero)
+    println(contaMolina.saldo)
 
-    println(contaAlex.titular)
-    println(contaAlex.numero)
-    println(contaAlex.saldo)
+    println(contaGustavo.titular)
+    println(contaGustavo.numero)
+    println(contaGustavo.saldo)
 
-    println("depositando na conta do Alex")
-    contaAlex.deposita(50.0)
-    println(contaAlex.saldo)
+    println("Depositando na conta do Gustavo")
+    contaGustavo.deposita(50.0)
+    println(contaGustavo.saldo)
 
-    println("depositando na conta da Fran")
-    contaFran.deposita(70.0)
-    println(contaFran.saldo)
+    println("Depositando na conta do Molina")
+    contaMolina.deposita(70.0)
+    println(contaMolina.saldo)
 
-    println("sacando na conta do Alex")
-    contaAlex.saca(250.0)
-    println(contaAlex.saldo)
+    println("Sacando na conta do Gustavo")
+    contaGustavo.saca(250.0)
+    println(contaGustavo.saldo)
 
-    println("sacando na conta da Fran")
-    contaFran.saca(100.0)
-    println(contaFran.saldo)
+    println("Sacando na conta do Molina")
+    contaMolina.saca(100.0)
+    println(contaMolina.saldo)
 
-    println("saque em excesso na conta do Alex")
-    contaAlex.saca(100.0)
-    println(contaAlex.saldo)
+    println("Saque em excesso na conta do Gustavo")
+    contaGustavo.saca(100.0)
+    println(contaGustavo.saldo)
 
-    println("saque em excesso na conta da Fran")
-    contaFran.saca(500.0)
-    println(contaFran.saldo)
+    println("Saque em excesso na conta do Molina")
+    contaMolina.saca(500.0)
+    println(contaMolina.saldo)
 
-    println("Transferência da conta da Fran para o Alex")
+    println("Transferência da conta do Molina para o Gustavo")
 
-    if (contaFran.transfere(destino = contaAlex, valor = 300.0)) {
+    if (contaMolina.transfere(destino = contaGustavo, valor = 300.0)) {
         println("Transferência sucedida")
     } else {
         println("Falha na transferência")
     }
 
-    println(contaAlex.saldo)
-    println(contaFran.saldo)
+    println(contaGustavo.saldo)
+    println(contaMolina.saldo)
 }
 
 class Conta(
